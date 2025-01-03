f301b629d3b6c37304c2f20b059a240cc588fcf5
sam-bdlabz <sam.varghese@bridgelabz.com>
2025-01-03 23:23:41 +0530

    Added code to extract data from sheet and store in csv/json

--- source/extract_reviews.py
+++ source/extract_reviews.py
@@ -1,11 +1,16 @@
 from auth import gsheet_client
 import pandas as pd
+import json
+import os
 
 # returns a list of reviews from the spreadsheet
 def extract_reviews():
     # Open the spreadsheet by its name or URL
-    SPREADSHEET_NAME = "Python-DailyStandup"
+    SPREADSHEET_NAME = "Copy of Digit Insurance-049- Feedback Survey (Responses)"
     sheet = gsheet_client.open(SPREADSHEET_NAME).sheet1  # Access the first sheet
+
+    with open("col_keys.json", "r") as f:
+        json_data = json.load(f)
 
     # Read data from the spreadsheet
     data = sheet.get_all_records()
@@ -13,13 +18,28 @@ def extract_reviews():
     # Convert to a Pandas DataFrame
     df = pd.DataFrame(data)
 
-    # Extract data from the "Review" column
-    if "Date" in df.columns:
-        review_list = df["Date"].tolist()
-        print("Review Column Data as List:")
-        print(review_list)
-        return review_list
-    else:
-        print("The 'Review' column does not exist in the spreadsheet.")
-        return None
+    # Get the keys of the JSON data
+    keys = json_data.keys() if isinstance(json_data, dict) else []
 
+    # Filter the DataFrame based on the keys
+    filtered_df = df[keys]
+    print(filtered_df)
+
+    # Select and rename the columns
+    renamed_df = filtered_df[keys].rename(columns=dict(json_data))
+
+    # # Write to DataFrame to a CSV file
+    file_name = "reviews_data.csv"
+    renamed_df.to_csv(file_name, mode='a', index=False, header=not os.path.exists(file_name))
+
+    # Write the DataFrame to a JSON file
+    file_name = "reviews_data.json"
+    renamed_df.to_json(file_name, orient="records", lines=False, indent=4)
+
+    # Extract data from the column data from the DataFrame
+    # review_list = df[keys].to_dict(orient='records')
+    # review_list = [
+    #     {json_data[key]: value for key, value in record.items()}
+    #     for record in df[keys].to_dict(orient='records')
+    # ]
+    # print(review_list)
